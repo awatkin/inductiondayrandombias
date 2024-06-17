754144fc3b32082860bd6370f4554406f7f950dc
Adam Watkin <adam.watkin@utcleeds.co.uk>
2024-06-17 12:10:17 +0100

    Base code written for learners to add to the code.

---
+++ main.py
@@ -0,0 +1,25 @@
+# Base code for the Induction day Random number bias task
+import random  # adds the random library into the software
+
+''' 
+Random Library Bias Assessment
+
+This is the base code for you to fill out and complete. 
+
+The Challenges to complete:
+
+1: Ask the user how many times they want to roll the dice, roll the dice that many times and report the results back. 
+
+2. Ask the user how many repeats of the test to do as well as the number of times to roll. Report the results
+
+3. Assess the results to say which number is the highest, by how much, the smallest number, and if you can, 
+if you feel the library is bias. 
+'''
+
+
+def main():  # main subroutine in which the code should be added
+    pass  # Comment this out when coding.
+
+
+if __name__ == '__main__':  # standard way of starting code
+    main()  # calls main to start the code
